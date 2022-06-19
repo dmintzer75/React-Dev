a76e1ab287243111afa1d12228cab4a1d121bf64
Dario Mintzer <dario.mintzer.ext@kavak.com>
2022-06-19 12:27:10 -0300

    Added robofriends project to repo

---
+++ robofriends/src/App.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Dario", lastName: "Mintzer" },
+      company: "ZTM",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello {this.state.name.firstName} {this.state.name.lastName}! I work
+            at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: { firstName: "Ariel", lastName: "Mintzer" },
+              });
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
